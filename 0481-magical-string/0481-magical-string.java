a9e9249613d4d8528f4eb0236d8cd265f20f0e8a
shivank-pundir <shivpundir1978@gmail.com>
2026-07-13 22:01:27 +0530

    Time: 3 ms (100%), Space: 44 MB (57.41%) - LeetHub

---
+++ 0481-magical-string/0481-magical-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int magicalString(int n) {
+        if (n <= 0) return 0;
+        if (n <= 3) return 1;
+
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 2;
+
+        int head = 2;
+        int tail = 3;
+        int num = 1;
+        int countOnes = 1;
+
+        while (tail < n) {
+            for (int i = 0; i < arr[head] && tail < n; i++) {
+                arr[tail] = num;
+                if (num == 1) {
+                    countOnes++;
+                }
+                tail++;
+            }
+            num = (num == 1) ? 2 : 1;
+            head++;
+        }
+
+        return countOnes;
+    }
+}
